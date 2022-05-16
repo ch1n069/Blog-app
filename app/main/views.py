928eb6f7d63626e76969fc751d6538156e690bad
bruno <brunolalpachino@gmail.com>
2022-05-16 10:34:59 +0300

    added the respective views for the forms in the views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,42 @@
 from flask import redirect,render_template,url_for
 from app.main import main
+from auth.forms import RegistrationForm , LoginForm
 #Views go here
+
+    
+
+
+
+@main.route('/register')
+def register():
+
+    form = RegistrationForm()
+
+
+    '''This is the home page for the application'''
+
+
+
+    return render_template('register.html' ,title='Register' , form=form)
+    
+
+
+@main.route('/login')
+def login():
+
+    form = LoginForm()
+
+
+    '''This is the home page for the application'''
+
+
+
+    return render_template('login.html' ,title='Login' , form=form)
+    
+
+
+
+
 @main.route('/')
 def index():
 
@@ -8,6 +44,6 @@ def index():
     '''This is the home page for the application'''
 
 
-    
+
     return render_template('index.html')
     
