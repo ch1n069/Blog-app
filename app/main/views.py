808e299acdef00c824132d91b0b2794bc1478f67
bruno <brunolalpachino@gmail.com>
2022-05-17 20:23:49 +0300

    ft == implemented delete feature

--- app/main/views.py
+++ app/main/views.py
@@ -158,3 +158,13 @@ def update_post(post_id):
     return render_template('create_post.html' , title= "update post", post=post ,forms=forms, legend='update post')
 
 
+@main.route('/post/<int:post_id>/delete', methods=[ 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been delete', 'success')
+    return redirect(url_for('main.index'))
